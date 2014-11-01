84814eb1b0215613859a212c5627030e9c7580a2
kazuhira <kazuhira.m@gmail.com>
2014-11-01 19:50:27 +0900

    remove META-INF, add Provider annotation to ContextResolver

--- resteasy-jackson2-scala/src/main/scala/org/littlewings/javaee7/rest/ScalaObjectMapperProvider.scala
+++ resteasy-jackson2-scala/src/main/scala/org/littlewings/javaee7/rest/ScalaObjectMapperProvider.scala
@@ -1,10 +1,15 @@
 package org.littlewings.javaee7.rest
 
-import javax.ws.rs.ext.ContextResolver
+import javax.ws.rs.{Consumes, Produces}
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.ext.{ContextResolver, Provider}
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
+@Provider
+@Consumes(Array(MediaType.APPLICATION_JSON))
+@Produces(Array(MediaType.APPLICATION_JSON))
 class ScalaObjectMapperProvider extends ContextResolver[ObjectMapper] {
   override def getContext(typ: Class[_]): ObjectMapper = {
     val objectMapper = new ObjectMapper
