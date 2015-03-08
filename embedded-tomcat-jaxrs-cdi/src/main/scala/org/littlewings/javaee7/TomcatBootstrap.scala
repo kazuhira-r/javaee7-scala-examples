b13e370557af25a7f846e65e84ed0cb78240555c
kazuhira <kazuhira.m@gmail.com>
2015-03-08 22:34:12 +0900

    fix, unexpected create catlina.home dir.

--- embedded-tomcat-jaxrs-cdi/src/main/scala/org/littlewings/javaee7/TomcatBootstrap.scala
+++ embedded-tomcat-jaxrs-cdi/src/main/scala/org/littlewings/javaee7/TomcatBootstrap.scala
@@ -9,25 +9,25 @@ import org.apache.tomcat.util.descriptor.web.ContextResource
 
 object TomcatBootsrap {
   def main(args: Array[String]): Unit = {
-
+    val port = 8080
     val tomcat = new Tomcat
 
     // ポートはデフォルトで8080
-    // tomcat.setPort(8080)
+    tomcat.setPort(port)
 
     try {
-      // 組み込みTomcatでJNDIを有効に
-      tomcat.enableNaming()
-
       // ベースのディレクトリ、DocbaseはSpring Bootを参考に
-      tomcat.setBaseDir(createTempDir(tomcat, "tomcat").getAbsolutePath)
+      tomcat.setBaseDir(createTempDir("tomcat", port).getAbsolutePath)
       val context =
-        tomcat.addWebapp("", createTempDir(tomcat, "tomcat-docbase").getAbsolutePath)
+        tomcat.addWebapp("", createTempDir("tomcat-docbase", port).getAbsolutePath)
   
       // CDIでWEB-INF/classesに配置されていなくても対象とされる、「flat」に設定
       context.addParameter("org.jboss.weld.environment.servlet.archive.isolation", "false")
       // RESTEasyとCDIの統合
       context.addParameter("resteasy.injector.factory", "org.jboss.resteasy.cdi.CdiInjectorFactory")
+
+      // 組み込みTomcatでJNDIを有効に
+      tomcat.enableNaming()
 
       // BeanManaerをJNDIリソースとして定義
       val resource = new ContextResource
@@ -52,8 +52,8 @@ object TomcatBootsrap {
     }
   }
 
-  def createTempDir(tomcat: Tomcat, prefix: String): File = {
-    val tempDir = File.createTempFile(s"${prefix}.", s".${tomcat.getConnector.getPort}")
+  def createTempDir(prefix: String, port: Int): File = {
+    val tempDir = File.createTempFile(s"${prefix}.", s".${port}")
     tempDir.delete()
     tempDir.mkdir()
     tempDir.deleteOnExit()
