9a61515a12ab584d4a5ed126f2a199cef42e4e0a
kazuhira <kazuhira.m@gmail.com>
2015-04-12 23:34:41 +0900

    add, @Typed annotation pattern.

--- cdi-constructor-injection/src/main/java/org/littlewings/javaee7/cdi/DecorationService.java
+++ cdi-constructor-injection/src/main/java/org/littlewings/javaee7/cdi/DecorationService.java
@@ -2,8 +2,10 @@ package org.littlewings.javaee7.cdi;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Typed;
 import javax.inject.Inject;
 
+/*
 // スコープアノテーションを変更
 // @ApplicationScoped
 @Dependent
@@ -17,6 +19,26 @@ public class DecorationService {
 
     // デフォルトコンストラクタは削除
     // public DecorationService() { }
+
+    public String join(String prefix, String suffix, String... tokens) {
+        return prefix + joiner.join(", ", tokens) + suffix;
+    }
+}
+*/
+
+public interface DecorationService {
+    String join(String prefix, String suffix, String... tokens);
+}
+
+@Typed(DecorationService.class)
+@ApplicationScoped
+class DecorationServiceImpl implements DecorationService {
+    private StringJoinService joiner;
+
+    @Inject
+    public DecorationServiceImpl(StringJoinService joiner) {
+        this.joiner = joiner;
+    }
 
     public String join(String prefix, String suffix, String... tokens) {
         return prefix + joiner.join(", ", tokens) + suffix;
