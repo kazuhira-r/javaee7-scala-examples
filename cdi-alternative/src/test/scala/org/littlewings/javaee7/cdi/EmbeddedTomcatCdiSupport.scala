2f2bc0b5fefb7633f9ab9cd918d7c397380b6d0f
kazuhira <kazuhira.m@gmail.com>
2015-04-19 02:14:36 +0900

    fix, remove temporary directories.

--- cdi-alternative/src/test/scala/org/littlewings/javaee7/cdi/EmbeddedTomcatCdiSupport.scala
+++ cdi-alternative/src/test/scala/org/littlewings/javaee7/cdi/EmbeddedTomcatCdiSupport.scala
@@ -10,14 +10,16 @@ import org.scalatest.BeforeAndAfterAll
 trait EmbeddedTomcatCdiSupport extends Suite with BeforeAndAfterAll {
   protected val port: Int = 8080
   protected val tomcat: Tomcat = new Tomcat
+  protected val baseDir: File = createTempDir("tomcat", port)
+  protected val docBaseDir: File = createTempDir("tomcat-docbase", port)
 
   override def beforeAll(): Unit = {
     tomcat.setPort(port)
 
-    tomcat.setBaseDir(createTempDir("tomcat", port).getAbsolutePath)
+    tomcat.setBaseDir(baseDir.getAbsolutePath)
 
     val context =
-      tomcat.addWebapp("", createTempDir("tomcat-docbase", port).getAbsolutePath)
+      tomcat.addWebapp("", docBaseDir.getAbsolutePath)
 
     context.addParameter("org.jboss.weld.environment.servlet.archive.isolation", "false")
     context.addParameter("resteasy.injector.factory", "org.jboss.resteasy.cdi.CdiInjectorFactory")
@@ -28,13 +30,28 @@ trait EmbeddedTomcatCdiSupport extends Suite with BeforeAndAfterAll {
   override def afterAll(): Unit = {
     tomcat.stop()
     tomcat.destroy()
+
+    deleteDirs(baseDir)
+    deleteDirs(docBaseDir)
   }
 
-  protected def createTempDir(prefix: String, port: Int): File = {
+  private def createTempDir(prefix: String, port: Int): File = {
     val tempDir = File.createTempFile(s"${prefix}.", s".${port}")
     tempDir.delete()
     tempDir.mkdir()
     tempDir.deleteOnExit()
     tempDir
+  }
+
+  private def deleteDirs(file: File): Unit = {
+    file
+      .listFiles
+      .withFilter(f => f.getName != "." && f.getName != "..")
+      .foreach {
+        case d if d.isDirectory => deleteDirs(d)
+        case f => f.delete()
+      }
+
+    file.delete()
   }
 }
