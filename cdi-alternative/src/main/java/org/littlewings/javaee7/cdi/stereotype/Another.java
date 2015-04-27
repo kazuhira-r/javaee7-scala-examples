6c47b800a5252f247f98e2a62bd8b4474efdff3f
kazuhira <kazuhira.m@gmail.com>
2015-04-28 00:18:41 +0900

    add, @Inherited annotation.

--- cdi-alternative/src/main/java/org/littlewings/javaee7/cdi/stereotype/Another.java
+++ cdi-alternative/src/main/java/org/littlewings/javaee7/cdi/stereotype/Another.java
@@ -1,6 +1,7 @@
 package org.littlewings.javaee7.cdi.stereotype;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -9,6 +10,7 @@ import javax.enterprise.inject.Alternative;
 import javax.enterprise.inject.Stereotype;
 
 @Alternative
+@Inherited
 @Stereotype
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
