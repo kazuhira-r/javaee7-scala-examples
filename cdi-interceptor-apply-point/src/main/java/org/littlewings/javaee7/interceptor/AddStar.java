44ea815f984a2c42d4c67f2fc80d7719a0007a67
kazuhira <kazuhira.m@gmail.com>
2015-05-17 21:23:41 +0900

    remove, Unused Priority Annotation.

--- cdi-interceptor-apply-point/src/main/java/org/littlewings/javaee7/interceptor/AddStar.java
+++ cdi-interceptor-apply-point/src/main/java/org/littlewings/javaee7/interceptor/AddStar.java
@@ -5,14 +5,11 @@ import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import javax.annotation.Priority;
-import javax.interceptor.Interceptor;
 import javax.interceptor.InterceptorBinding;
 
 @InterceptorBinding
 @Inherited
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-@Priority(Interceptor.Priority.APPLICATION)
 public @interface AddStar {
 }
